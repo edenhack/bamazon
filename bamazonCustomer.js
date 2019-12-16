18681449549293a0cf222cbac434b4f9cd7043e3
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-12-15 21:43:11 -0600

    added gitignore, installed mysql, table, created table for viewing products

---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+const {table} = require("table");
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "trif3ktac0dekta",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as customer id " + connection.threadId + "\n");
+    customerDisplay();
+});
+
+function customerDisplay() {
+    console.log("Listing products for sale....\n");
+    const query = connection.query(
+        "SELECT * FROM products",
+        function (err, res) {
+            if (err) throw err;
+            const data = res.map((product) => [product.product_id, product.product_name, product.department_name, product.price_customer, product.stock_quantity]);
+            const tableData = [
+                ["product_id", "product_name", "department_name", "price_customer", "stock_quantity"],
+                ...data
+            ];
+            console.log(table(tableData));
+            connection.end();
+        });
+}
