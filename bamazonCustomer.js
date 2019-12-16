cdd5c0b43d76bb306547b15111f06dbf2cca568f
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-12-15 22:41:31 -0600

    removed localhost password for mysql database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "trif3ktac0dekta",
+    password: "",
     database: "bamazon_db"
 });
 
